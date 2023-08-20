dd96c9022cbc1cc3c998b0600aae525d1f231372
Tejas Mishr <113692600+TejasMishr@users.noreply.github.com>
2023-08-20 11:24:28 +0530

    Add files via upload

---
+++ Resources/script.js
@@ -0,0 +1,10 @@
+$("#search-icon").click(function() {
+    $(".nav").toggleClass("search");
+    $(".nav").toggleClass("no-search");
+    $(".search-input").toggleClass("search-active");
+  });
+  
+  $('.menu-toggle').click(function(){
+     $(".nav").toggleClass("mobile-nav");
+     $(this).toggleClass("is-active");
+  });
