b9ad16f8de3881227e9233ad0fa9db381dcd2f7f
TejasMishr <tejasmishra72@gmail.com>
2023-09-11 21:27:19 +0530

    topbar report ok

--- topbar/topbar.js
+++ topbar/topbar.js
@@ -16,15 +16,43 @@ function createUpdateLink(text, href) {
     // Append the image and link to the updates container
     var updatesContainer = document.getElementById('updates-container');
     updatesContainer.appendChild(img);
-    updatesContainer.appendChild(document.createTextNode('\u00A0')); // Add a non-breaking space
+    updatesContainer.appendChild(document.createTextNode('\u00A0')); // Added non-breaking space
     updatesContainer.appendChild(updateLink);
-    updatesContainer.appendChild(document.createTextNode('\u00A0|\u00A0')); // Add pipe separator
+    updatesContainer.appendChild(document.createTextNode('\u00A0|\u00A0')); // Added pipe separator
   }
   
+
+
+
+  function createUpdateExtraLink(text, href) {
+    var updateLink = document.createElement('a');
+    updateLink.textContent = text;
+    updateLink.href = href;
+    updateLink.target = '_blank';
+    updateLink.style.color = 'Red';
+    updateLink.style.fontSize = '15px';
+    updateLink.style.fontWeight = 'bold';
+    
+    // Create an image element
+    var img = document.createElement('img');
+    img.src = 'img/warn.png';
+    img.style.width = '22px';
+  
+    // Append the image and link to the updates container
+    var updatesContainer = document.getElementById('updates-container');
+    updatesContainer.appendChild(img);
+    updatesContainer.appendChild(document.createTextNode('\u00A0')); // Added non-breaking space
+    updatesContainer.appendChild(updateLink);
+    updatesContainer.appendChild(document.createTextNode('\u00A0|\u00A0')); // Added pipe separator
+  }
+
+
   // Call the createUpdateLink function to add updates
+  createUpdateExtraLink('Report Issues', '../report.html');
   createUpdateLink('Civil old Notes Added', 'pages/pages.html');
   createUpdateLink('IT Content Updated', '#');
   createUpdateLink('B-pharma Sem 1 Added', '#');
   createUpdateLink('MCA BBA MBA department Added', '#');
+
   
   
