e5f52a79046597849be410f768e336de54f71c30
TejasMishr <tejasmishra72@gmail.com>
2023-09-24 12:38:57 +0530

    alignment fixed all pages

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //     $(".nav").toggleClass("no-search");
 //     $(".search-input").toggleClass("search-active");
 //   });
-  
+
 //   $('.menu-toggle').click(function(){
 //      $(".nav").toggleClass("mobile-nav");
 //      $(this).toggleClass("is-active");
@@ -28,26 +28,26 @@ var menu = document.querySelector('#mobile-menu');
 var count = 0;
 
 
-var f1 = ()=>{
+var f1 = () => {
     // alert('ok');
-   count++;
-   
-     
-   if(count%2==0){
-    // alert("ok");
-    navi.style.transform = "translate(-101%)";
+    count++;
 
-    bari2.style.display="flex";
-    bari1.style.transform="rotate(0) translateY(0px)";
-    bari3.style.transform="rotate(0)  translateY(0px)";
-   }else{
-    
-    navi.style.display="flex";
-    navi.style.transform = "translate(0%)";
-    bari2.style.display="none";
-    bari1.style.transform="rotate(45deg) translateY(6px)";
-    bari3.style.transform="rotate(-45deg)  translateY(-5px)";
-   }
+
+    if (count % 2 == 0) {
+        // alert("ok");
+        navi.style.transform = "translate(-101%)";
+
+        bari2.style.display = "flex";
+        bari1.style.transform = "rotate(0) translateY(0px)";
+        bari3.style.transform = "rotate(0)  translateY(0px)";
+    } else {
+
+        navi.style.display = "flex";
+        navi.style.transform = "translate(0%)";
+        bari2.style.display = "none";
+        bari1.style.transform = "rotate(45deg) translateY(6px)";
+        bari3.style.transform = "rotate(-45deg)  translateY(-5px)";
+    }
 
 
 
@@ -56,7 +56,9 @@ var f1 = ()=>{
 
 // marquee bug fixing
 // <!-- block right -->
-document.addEventListener("contextmenu",function(e){e.preventDefault();})
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+})
 
 window.addEventListener('keydown', function (event) {
 
@@ -64,9 +66,9 @@ window.addEventListener('keydown', function (event) {
 
 });
 
-window.onresize = function() {
+window.onresize = function () {
     if ((window.outerHeight - window.innerHeight) > 100) {
-     window.close();
+        window.close();
     }
 }
 
@@ -76,15 +78,15 @@ window.onresize = function() {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     const exploreResources = document.getElementById("explore-resources");
 
-    
+
     exploreResources.addEventListener("click", function () {
         // Calculate the scroll position to scroll by 98%
         const scrollPosition = window.innerHeight * 1;
 
-        
+
         window.scrollTo({
             top: scrollPosition,
             behavior: "smooth", //smoooth scroll
