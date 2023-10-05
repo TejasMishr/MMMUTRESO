34bd49ce2d7d5dc973d026750a2cb9b0ed714e62
AdityaGaur7 <adgaur027@gmail.com>
2023-10-05 23:10:24 +0530

    lecture button added

---
+++ Bpharm/Lecture/Playlist.js
@@ -0,0 +1,256 @@
+const Pid = [
+  // Branch 0 (Bpharma)
+  [
+    // Semester (I)
+    [
+      // Subject 0 (sub I)
+      "null",
+      // Subject 1 (sub II)
+      "null",
+      // Subject 2 (sub III)
+      "null",
+      // Subject 3 (sub IV)
+      "null",
+      // Subject 4 (sub V)
+      "null",
+      // Subject 5 (sub VI)
+      "null",
+    ],
+    // Semester 2
+    [
+      // Subject 0 (sub I)
+      "null",
+      // Subject 1 (sub II)
+      "null",
+      // Subject 2 (sub III)
+      "null",
+      // Subject 3 (sub IV)
+      "null",
+      // Subject 4 (sub V)
+      "null",
+      // Subject 5 (sub VI)
+      "null",
+    ],
+    //Sem 3
+    [
+      // Subject 0 (sub I)
+      "null",
+      // Subject 1 (sub II)
+      "null",
+      // Subject 2 (sub III)
+      "null",
+      // Subject 3 (sub IV)
+      "null",
+      // Subject 4 (sub V)
+      "null",
+      // Subject 5 (sub VI)
+      "null",
+    ],
+
+    // Semester 4  (--->>>>  DONE <<<<<------)
+    [
+      // Subject 0 (sub I)
+      "null",
+      // Subject 1 (sub II)
+      "null",
+      // Subject 2 (sub III)
+      "null",
+      // Subject 3 (sub IV)
+      "null",
+      // Subject 4 (sub V)
+      "null",
+      // Subject 5 (sub VI)
+      "null",
+    ],
+    // Semester 5
+    [
+      // Subject 0 (sub I)
+      "null",
+      // Subject 1 (sub II)
+      "null",
+      // Subject 2 (sub III)
+      "null",
+      // Subject 3 (sub IV)
+      "null",
+      // Subject 4 (sub V)
+      "null",
+      // Subject 5 (sub VI)
+      "null",
+    ],
+    // Semester 6
+    [
+      // Subject 0 (sub I)
+      "null",
+      // Subject 1 (sub II)
+      "null",
+      // Subject 2 (sub III)
+      "null",
+      // Subject 3 (sub IV)
+      "null",
+      // Subject 4 (sub V)
+      "null",
+      // Subject 5 (sub VI)
+      "null",
+    ],
+    // Semester 7
+    [
+      // Subject 0 (sub I)
+      "null",
+      // Subject 1 (sub II)
+      "null",
+      // Subject 2 (sub III)
+      "null",
+      // Subject 3 (sub IV)
+      "null",
+      // Subject 4 (sub V)
+      "null",
+      // Subject 5 (sub VI)
+      "null",
+    ],
+    // Semester 8
+    [
+      // Subject 0 (sub I)
+      "null",
+      // Subject 1 (sub II)
+      "null",
+      // Subject 2 (sub III)
+      "null",
+      // Subject 3 (sub IV)
+      "null",
+      // Subject 4 (sub V)
+      "null",
+      // Subject 5 (sub VI)
+      "null",
+    ]
+  ]
+]
+
+
+
+
+// const Pid = [
+//   // Branch 0 (Bpharma)
+//   [
+//     // Semester (I)
+//     [
+//       // Subject 0 (sub I)
+//       "null",
+//       // Subject 1 (sub II)
+//       "null",
+//       // Subject 2 (sub III)
+//       "null",
+//       // Subject 3 (sub IV)
+//       "null",
+//       // Subject 4 (sub V)
+//       "null",
+//       // Subject 5 (sub VI)
+//       "null",
+//     ],
+//     // Semester 2
+//     [
+//       // Subject 0 (sub I)
+//       "null",
+//       // Subject 1 (sub II)
+//       "null",
+//       // Subject 2 (sub III)
+//       "null",
+//       // Subject 3 (sub IV)
+//       "null",
+//       // Subject 4 (sub V)
+//       "null",
+//       // Subject 5 (sub VI)
+//       "null",
+//     ],
+//     //Sem 3
+//     [
+//       // Subject 0 (sub I)
+//       "null",
+//       // Subject 1 (sub II)
+//       "null",
+//       // Subject 2 (sub III)
+//       "null",
+//       // Subject 3 (sub IV)
+//       "null",
+//       // Subject 4 (sub V)
+//       "null",
+//       // Subject 5 (sub VI)
+//       "null",
+//     ],
+
+//     // Semester 4  (--->>>>  DONE <<<<<------)
+//     [
+//       // Subject 0 (sub I)
+//       "null",
+//       // Subject 1 (sub II)
+//       "null",
+//       // Subject 2 (sub III)
+//       "null",
+//       // Subject 3 (sub IV)
+//       "null",
+//       // Subject 4 (sub V)
+//       "null",
+//       // Subject 5 (sub VI)
+//       "null",
+//     ],
+//     // Semester 5
+//     [
+//       // Subject 0 (sub I)
+//       "null",
+//       // Subject 1 (sub II)
+//       "null",
+//       // Subject 2 (sub III)
+//       "null",
+//       // Subject 3 (sub IV)
+//       "null",
+//       // Subject 4 (sub V)
+//       "null",
+//       // Subject 5 (sub VI)
+//       "null",
+//     ],
+//     // Semester 6
+//     [
+//       // Subject 0 (sub I)
+//       "null",
+//       // Subject 1 (sub II)
+//       "null",
+//       // Subject 2 (sub III)
+//       "null",
+//       // Subject 3 (sub IV)
+//       "null",
+//       // Subject 4 (sub V)
+//       "null",
+//       // Subject 5 (sub VI)
+//       "null",
+//     ],
+//     // Semester 7
+//     [
+//       // Subject 0 (sub I)
+//       "null",
+//       // Subject 1 (sub II)
+//       "null",
+//       // Subject 2 (sub III)
+//       "null",
+//       // Subject 3 (sub IV)
+//       "null",
+//       // Subject 4 (sub V)
+//       "null",
+//       // Subject 5 (sub VI)
+//       "null",
+//     ],
+//     // Semester 8
+//     [
+//       // Subject 0 (sub I)
+//       "null",
+//       // Subject 1 (sub II)
+//       "null",
+//       // Subject 2 (sub III)
+//       "null",
+//       // Subject 3 (sub IV)
+//       "null",
+//       // Subject 4 (sub V)
+//       "null",
+//       // Subject 5 (sub VI)
+//       "null",
+//     ]
+//   ]
+// ]
